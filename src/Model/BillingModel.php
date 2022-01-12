5df0e4068d933132db843f5825fd3a8694e0091d
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-12 15:34:27 +0100

    create models & serialization declarations

---
+++ src/Model/BillingModel.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusIngPlugin\Model;
+
+final class BillingModel
+{
+    private string $firstName;
+
+    private string $lastName;
+
+    private string $company;
+
+    private string $street;
+
+    private string $city;
+
+    private string $region;
+
+    private string $postalCode;
+
+    private string $countryCodeAlpha2;
+
+    public function __construct(
+        string $firstName,
+        string $lastName,
+        string $company,
+        string $street,
+        string $city,
+        string $region,
+        string $postalCode,
+        string $countryCodeAlpha2
+    ) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->company = $company;
+        $this->street = $street;
+        $this->city = $city;
+        $this->region = $region;
+        $this->postalCode = $postalCode;
+        $this->countryCodeAlpha2 = $countryCodeAlpha2;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getCompany(): string
+    {
+        return $this->company;
+    }
+
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    public function getCountryCodeAlpha2(): string
+    {
+        return $this->countryCodeAlpha2;
+    }
+
+    public function getJsonFromObject(self $billingModel)
+    {
+    }
+}
---
+++ src/Model/IngHeaderModel.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusIngPlugin\Model;
+
+final class IngHeaderModel
+{
+    private string $accept = 'application/json';
+
+    private string $contentType = 'application/json';
+
+    private string $authorization = 'Bearer j1hdp8u2u4e2mep1uzepx29d8ymy77x4dycs1tyx2dwgswjdcdwlo2jgcb3yagf2';
+}
