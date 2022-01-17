603975846ac1fd81e261e261b01844a8be600c84
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-17 14:13:18 +0100

    create ConfigurationType & declare in xml & add translations

---
+++ src/Form/Type/ConfigurationType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusIngPlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('token', TextType::class, [
+                'label' => 'bitbag_sylius_ing_plugin.ui.token',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'bitbag_sylius_ing_plugin.password.not_blank',
+                        'groups' => ['sylius'],
+                    ]),
+                ],
+            ])
+            ->add('redirect', CheckboxType::class, [
+                'label' => 'bitbag_sylius_ing_plugin.ui.redirect',
+            ]);
+    }
+}
