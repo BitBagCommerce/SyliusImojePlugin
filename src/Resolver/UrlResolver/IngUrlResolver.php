b08c299e68acf408e41f86ff629bb93cb6fca922
Piotr <PiotrekSz-18@wp.pl>
2022-01-20 10:26:53 +0100

    add headers to IngApiClient & refactor code

--- src/Resolver/UrlResolver/IngUrlResolver.php
+++ src/Resolver/UrlResolver/IngUrlResolver.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusIngPlugin\Resolver\UrlResolver;
 
-use BitBag\SyliusIngPlugin\Provider\IngClientConfigurationProviderInterface;
+use BitBag\SyliusIngPlugin\Configuration\IngClientConfigurationInterface;
 
 final class IngUrlResolver implements IngUrlResolverInterface
 {
-    public function buildUrl(string $code, IngClientConfigurationProviderInterface $clientConfigurationProvider): string
+    public function buildUrl(string $code, IngClientConfigurationInterface $clientConfiguration): string
     {
-        $baseUrl = '';
-
-        $clientConfiguration = $clientConfigurationProvider->getPaymentMethodConfiguration($code);
-
         if ($clientConfiguration->isProd() === true) {
             $baseUrl = $clientConfiguration->getProdUrl();
         } else {
             $baseUrl = $clientConfiguration->getSandboxUrl();
         }
 
-        return sprintf('%s/%s/', $baseUrl, $clientConfiguration->getMerchantId());
+        return \sprintf('%s/%s/', $baseUrl, $clientConfiguration->getMerchantId());
     }
 }
