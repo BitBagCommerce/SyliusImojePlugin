4ed8ab70ddf367da6b68fada4aab3faac2805647
Piotr <PiotrekSz-18@wp.pl>
2022-01-20 12:43:32 +0100

    remove unnecessary factory & create logic for controller

--- src/Controller/Shop/InitializePaymentController.php
+++ src/Controller/Shop/InitializePaymentController.php
@@ -4,21 +4,64 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusIngPlugin\Controller\Shop;
 
-use BitBag\SyliusIngPlugin\Provider\IngClientConfigurationProviderInterface;
+use BitBag\SyliusIngPlugin\Model\BillingModel;
+use BitBag\SyliusIngPlugin\Model\CustomerModel;
+use BitBag\SyliusIngPlugin\Model\ShippingModel;
+use BitBag\SyliusIngPlugin\Model\TransactionModel;
+use BitBag\SyliusIngPlugin\Provider\IngClientProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class InitializePaymentController
 {
-    private IngClientConfigurationProviderInterface $clientConfigurationProvider;
+    private IngClientProviderInterface $ingClientProvider;
 
-    public function __construct(IngClientConfigurationProviderInterface $clientConfigurationProvider)
+    public function __construct(IngClientProviderInterface $ingClientProvider)
     {
-        $this->clientConfigurationProvider = $clientConfigurationProvider;
+        $this->ingClientProvider = $ingClientProvider;
     }
 
     public function __invoke(Request $request): Response
     {
+        $customerModel = new CustomerModel('','','','','','',);
+        $billingModel = new BillingModel('',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+        );
+
+        $shippingModel = new ShippingModel('',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+        );
+
+        $transactionModel = new TransactionModel('',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            $customerModel,
+            $billingModel,
+            $shippingModel
+        );
+
+        $client = $this->ingClientProvider->getClient('ing_code');
+        $response = $client->createTransaction($transactionModel);
+
         return new Response();
     }
 }
