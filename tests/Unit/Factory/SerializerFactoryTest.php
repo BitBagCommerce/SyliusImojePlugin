e35759431c35eee4042a398589d627f8c5e87d5e
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-25 15:26:22 +0100

    add test for SerializerFactory

---
+++ tests/Unit/Factory/SerializerFactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Factory;
+
+use BitBag\SyliusIngPlugin\Factory\Serializer\SerializerFactory;
+use BitBag\SyliusIngPlugin\Factory\Serializer\SerializerFactoryInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class SerializerFactoryTest extends TestCase
+{
+    private SerializerFactoryInterface $serializerFactory;
+
+    protected function setUp(): void
+    {
+        $this->serializerFactory = new SerializerFactory();
+    }
+
+    public function test_return_serializer(): void
+    {
+
+        $this->assertInstanceOf(
+            SerializerInterface::class,
+            $this->serializerFactory->createSerializerWithNormalizer()
+        );
+    }
+}
