ed1ec350faf31c2a1fa26e2b3087b629cc5f70c4
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-25 10:15:23 +0100

    fix github actions

--- src/Resolver/Order/OrderResolver.php
+++ src/Resolver/Order/OrderResolver.php
@@ -33,7 +33,7 @@ final class OrderResolver implements OrderResolverInterface
 
     private function getCurrentRequest(): Request
     {
-        $result = $this->requestStack->getMainRequest();
+        $result = $this->requestStack->getMasterRequest();
 
         if ($result === null) {
             throw new MissingRequestException();
