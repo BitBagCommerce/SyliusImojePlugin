ffcf859c6dd59b74ca1bf5ce2e8f55a678decfe6
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-25 14:29:53 +0100

    add test for IngApiClient with exception

--- tests/Unit/Client/IngApiClientTest.php
+++ tests/Unit/Client/IngApiClientTest.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Unit\Client;
 
 use BitBag\SyliusIngPlugin\Client\IngApiClient;
+use BitBag\SyliusIngPlugin\Exception\IngBadRequestException;
 use BitBag\SyliusIngPlugin\Factory\Serializer\SerializerFactoryInterface;
 use BitBag\SyliusIngPlugin\Model\TransactionModelInterface;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -60,6 +62,41 @@ final class IngApiClientTest extends TestCase
             ->method('post')
             ->with('url/transaction', $parameters)
             ->willReturn(new Response());
+
+        $this->ingApiClient->createTransaction($transactionModel);
+    }
+
+    public function test_create_transaction_with_Exception(): void
+    {
+        $serializer = $this->createMock(SerializerInterface::class);
+        $transactionModel = $this->createMock(TransactionModelInterface::class);
+        $exception = $this->createMock(GuzzleException::class);
+
+        $parameters['body'] = 'model';
+        $parameters['headers'] = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer token'
+        ];
+
+        $this->serializerFactory
+            ->expects($this->once())
+            ->method('createSerializerWithNormalizer')
+            ->willReturn($serializer);
+
+        $serializer
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($transactionModel, 'json')
+            ->willReturn('model');
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('post')
+            ->with('url/transaction', $parameters)
+            ->will($this->throwException($exception));
+
+        $this->expectException(IngBadRequestException::class);
 
         $this->ingApiClient->createTransaction($transactionModel);
     }
