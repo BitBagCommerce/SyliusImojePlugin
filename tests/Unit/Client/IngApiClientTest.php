9421326b4a668432f032e4b77195dc95c57a6dc7
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-25 14:12:28 +0100

    add test for IngApiClient

---
+++ tests/Unit/Client/IngApiClientTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Client;
+
+use BitBag\SyliusIngPlugin\Client\IngApiClient;
+use BitBag\SyliusIngPlugin\Factory\Serializer\SerializerFactoryInterface;
+use BitBag\SyliusIngPlugin\Model\TransactionModelInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class IngApiClientTest extends TestCase
+{
+    /** @var Client|MockObject */
+    private object $httpClient;
+
+    /** @var SerializerFactoryInterface|MockObject */
+    private object $serializerFactory;
+
+    private IngApiClient $ingApiClient;
+
+    protected function setUp(): void
+    {
+        $this->httpClient = $this->getMockBuilder(Client::class)->addMethods(['post'])->getMock();
+        $this->serializerFactory = $this->createMock(SerializerFactoryInterface::class);
+        $token = "token";
+        $url = "url/";
+        $this->ingApiClient = new IngApiClient( $this->httpClient, $this->serializerFactory, $token,$url);
+    }
+
+    public function test_create_transaction(): void
+    {
+        $serializer = $this->createMock(SerializerInterface::class);
+        $transactionModel = $this->createMock(TransactionModelInterface::class);
+
+        $parameters['body'] = 'model';
+        $parameters['headers'] = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer token'
+        ];
+
+        $this->serializerFactory
+            ->expects($this->once())
+            ->method('createSerializerWithNormalizer')
+            ->willReturn($serializer);
+
+        $serializer
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($transactionModel, 'json')
+            ->willReturn('model');
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('post')
+            ->with('url/transaction', $parameters)
+            ->willReturn(new Response());
+
+        $this->ingApiClient->createTransaction($transactionModel);
+    }
+}
