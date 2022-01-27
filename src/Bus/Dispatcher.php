bf4978eabd88c9a58d43c72c0ac58cb8bc1a0c67
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-27 10:16:24 +0100

    add command & handler to change order state

---
+++ src/Bus/Dispatcher.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusIngPlugin\Bus;
+
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class Dispatcher implements DispatcherInterface
+{
+    use HandleTrait;
+
+    /** @var MessageBusInterface */
+    private $messageBus;
+
+    public function __construct(
+        MessageBusInterface $messageBus
+    ) {
+        $this->messageBus = $messageBus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function dispatch(object $action)
+    {
+        return $this->handle($action);
+    }
+}
---
+++ src/Bus/Handler/FinalizeOrderHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusIngPlugin\Bus\Handler;
+
+use BitBag\SyliusIngPlugin\Bus\Command\FinalizeOrder;
+use SM\Factory\FactoryInterface;
+use Sylius\Component\Core\OrderCheckoutTransitions;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+final class FinalizeOrderHandler implements MessageHandlerInterface
+{
+    /** @var FactoryInterface */
+    private $stateMachineFactory;
+
+    /** @var RepositoryInterface */
+    private $orderRepository;
+
+    public function __construct(
+        FactoryInterface $stateMachineFactory,
+        RepositoryInterface $orderRepository
+    ) {
+        $this->stateMachineFactory = $stateMachineFactory;
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function __invoke(FinalizeOrder $command): void
+    {
+        $order = $command->getOrder();
+
+        $stateMachine = $this->stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
+        $stateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE, true);
+
+        $this->orderRepository->add($order);
+    }
+}
