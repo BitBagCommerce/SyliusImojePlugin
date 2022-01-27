0742ea415c21c494d41bc636166866279d2df246
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-01-27 11:54:53 +0100

    add complierPass

--- src/SyliusIngPlugin.php
+++ src/SyliusIngPlugin.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusIngPlugin;
 
+use BitBag\SyliusIngPlugin\DependencyInjection\CompilerPass\MessageBusPolyfillPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
 final class SyliusIngPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(
+            new MessageBusPolyfillPass(),
+            PassConfig::TYPE_BEFORE_OPTIMIZATION,
+            1
+        );
+    }
 }
