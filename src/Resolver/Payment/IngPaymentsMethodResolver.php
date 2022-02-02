033d0bd9612a4e6c926c6682e2200384161d33a2
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-02-02 13:21:27 +0100

    add logic to resolver

--- src/Resolver/Payment/IngPaymentsMethodResolver.php
+++ src/Resolver/Payment/IngPaymentsMethodResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusIngPlugin\Resolver\Payment;
 
-
 use BitBag\SyliusIngPlugin\Repository\PaymentMethodRepository;
 
 final class IngPaymentsMethodResolver implements IngPaymentsMethodResolverInterface
@@ -16,11 +15,35 @@ final class IngPaymentsMethodResolver implements IngPaymentsMethodResolverInterf
         $this->paymentMethodRepository = $paymentMethodRepository;
     }
 
-
     public function resolve(): ?array
     {
         $data = [];
         $data['data'] = $this->paymentMethodRepository->getOneForIng()->getGatewayConfig()->getConfig();
+
+        if (array_key_exists('isProd', $data['data'])) {
+            unset($data['data']['isProd']);
+        }
+
+        if (array_key_exists('token', $data['data'])) {
+            unset($data['data']['token']);
+        }
+
+        if (array_key_exists('prodUrl', $data['data'])) {
+            unset($data['data']['prodUrl']);
+        }
+
+        if (array_key_exists('redirect', $data['data'])) {
+            unset($data['data']['redirect']);
+        }
+
+        if (array_key_exists('sandboxUrl', $data['data'])) {
+            unset($data['data']['sandboxUrl']);
+        }
+
+        if (array_key_exists('merchantId', $data['data'])) {
+            unset($data['data']['merchantId']);
+        }
+
         return $data;
     }
 }
