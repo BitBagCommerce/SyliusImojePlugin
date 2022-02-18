33c3c41b1542cf0ee7ad6658113bb0d74d3b9710
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-02-18 12:07:00 +0100

    change json objects to array & add test

--- src/Resolver/TransactionData/TransactionDataResolver.php
+++ src/Resolver/TransactionData/TransactionDataResolver.php
@@ -11,11 +11,11 @@ final class TransactionDataResolver implements TransactionDataResolverInterface
     public function resolve(ResponseInterface $response): array
     {
         $transactionData = [];
-        $data = json_decode($response->getBody()->getContents());
-        $transactionData['transactionId'] = $data->transaction->id;
-        $transactionData['serviceId'] = $data->transaction->serviceId;
-        $transactionData['orderId'] = $data->transaction->orderId;
-        $transactionData['paymentUrl'] = $data->action->url;
+        $data = json_decode($response->getBody()->getContents(), true);
+        $transactionData['transactionId'] = $data['transaction']['id'];
+        $transactionData['serviceId'] = $data['transaction']['serviceId'];
+        $transactionData['orderId'] = $data['transaction']['orderId'];
+        $transactionData['paymentUrl'] = $data['action']['url'];
 
         return $transactionData;
     }
