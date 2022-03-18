2acc0785e5f5f48c9f879d6515b5c40227e02e9b
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2022-03-18 12:37:58 +0100

    fix after cr and add images to payment methods

--- src/Form/Extension/CompleteTypeExtension.php
+++ src/Form/Extension/CompleteTypeExtension.php
@@ -17,6 +17,8 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class CompleteTypeExtension extends AbstractTypeExtension
 {
+    public const BLIK_LENGTH = 6;
+
     private OrderResolverInterface $orderResolver;
 
     private OrderPaymentResolverInterface $paymentResolver;
@@ -49,8 +51,8 @@ final class CompleteTypeExtension extends AbstractTypeExtension
                                 'groups' => ['sylius'],
                             ]),
                             new Length([
-                                'min' => 6,
-                                'max' => 6,
+                                'min' => self::BLIK_LENGTH,
+                                'max' => self::BLIK_LENGTH,
                                 'groups' => ['sylius'],
                             ]),
                         ],
