aadffee28b0d305b6d8e14bdf1b52820c2d1972f
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2022-12-01 07:48:02 +0100

    Fix unit tests

--- tests/Unit/Resolver/Signature/OwnSignatureResolverTest.php
+++ tests/Unit/Resolver/Signature/OwnSignatureResolverTest.php
@@ -17,7 +17,7 @@ final class OwnSignatureResolverTest extends TestCase
 {
     private const PRIVATE_KEY = 'ShopKey';
 
-    private const FACTORY_NAME = 'bitbag_imoje';
+    private const FACTORY_NAME = 'BitBag_imoje';
 
     private RequestStack $requestStack;
 
