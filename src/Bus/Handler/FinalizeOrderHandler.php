99b72f3f0cd1063f8bed7eedc6877a4fd4101c00
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2022-12-01 11:47:10 +0100

    fix order confirmation email

--- src/Bus/Handler/FinalizeOrderHandler.php
+++ src/Bus/Handler/FinalizeOrderHandler.php
@@ -6,31 +6,49 @@ namespace BitBag\SyliusIngPlugin\Bus\Handler;
 
 use BitBag\SyliusIngPlugin\Bus\Command\FinalizeOrder;
 use SM\Factory\FactoryInterface;
+use Sylius\Bundle\ApiBundle\Command\Checkout\SendOrderConfirmation;
+use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\OrderCheckoutTransitions;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 final class FinalizeOrderHandler implements MessageHandlerInterface
 {
+    private const CART_STATE = 'cart';
+
     private FactoryInterface $stateMachineFactory;
 
     private RepositoryInterface $orderRepository;
 
+    private MessageBusInterface $commandBus;
+
     public function __construct(
         FactoryInterface $stateMachineFactory,
-        RepositoryInterface $orderRepository
+        RepositoryInterface $orderRepository,
+        MessageBusInterface $commandBus
     ) {
         $this->stateMachineFactory = $stateMachineFactory;
         $this->orderRepository = $orderRepository;
+        $this->commandBus = $commandBus;
     }
 
     public function __invoke(FinalizeOrder $command): void
     {
         $order = $command->getOrder();
-
+        $state = $order->getState();
         $stateMachine = $this->stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
         $stateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE, true);
-
         $this->orderRepository->add($order);
+
+        $this->sendOrderConfirmationEmail($order, $state);
+    }
+
+    private function sendOrderConfirmationEmail(OrderInterface $order, string $state): void
+    {
+        $token = $order->getTokenValue();
+        if ($token && $state === self::CART_STATE) {
+            $this->commandBus->dispatch(new SendOrderConfirmation($token));
+        }
     }
 }
